3b09b7d68d9c16670e594f7f6804a02b8da5b752
Helena Paixão <hp.helenapaixao@gmail.com>
2020-06-10 18:43:11 -0400

    Desafio finalizado

--- src/pages/Import/index.tsx
+++ src/pages/Import/index.tsx
@@ -22,20 +22,29 @@ const Import: React.FC = () => {
   const [uploadedFiles, setUploadedFiles] = useState<FileProps[]>([]);
   const history = useHistory();
 
+  function uploadSingleFile(file: FileProps): Promise<void> {
+    const data = new FormData();
+    data.append('file', file.file);
+    return api.post('/transactions/import', data);
+  }
+
   async function handleUpload(): Promise<void> {
-    // const data = new FormData();
-
-    // TODO
-
     try {
-      // await api.post('/transactions/import', data);
+      await Promise.all(uploadedFiles.map(file => uploadSingleFile(file)));
+      history.push('/');
     } catch (err) {
-      // console.log(err.response.error);
+      console.log(err.response.error);
     }
   }
 
   function submitFile(files: File[]): void {
-    // TODO
+    const formattedFiles = files.map(file => ({
+      file,
+      name: file.name,
+      readableSize: filesize(file.size),
+    }));
+
+    setUploadedFiles([...uploadedFiles, ...formattedFiles]);
   }
 
   return (
